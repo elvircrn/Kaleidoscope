647b8e155ace9276390b54815491f6472ae7a5bb
elvircrn <elvircrn@gmail.com>
2019-09-28 21:56:33 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/simple-tool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple-tool.dir/main.cpp.o"
+  "simple-tool"
+  "simple-tool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple-tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+enum Token {
+    tok_eof = -1,
+    tok_def = -2,
+
+
+};
+
+int main() {
+    return 0;
+}
